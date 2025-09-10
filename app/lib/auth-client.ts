eca3a5f566f684aa87b0717a83feccc5859721bc
Nayan Gallego Sivaraman <nayan@Vijays-MacBook-Pro.local>
2025-09-11 02:00:42 +1000

    fixing env variab les

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.VERCEL_URL,
+    baseURL: "https://upreview-six.vercel.app",
 })
