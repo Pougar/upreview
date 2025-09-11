f9ff92ddf480ec3e6e97ee2b41ed3667bb7a8f60
Nayan Gallego Sivaraman <nayan@Vijays-MacBook-Pro.local>
2025-09-11 14:22:24 +1000

    changed to pg not postgres

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -9,7 +9,12 @@ export const auth = betterAuth({
     database: pool,
     emailAndPassword: {
         enabled: true, 
-    }
+    },
+        trustedOrigins: [
+        "http://localhost:3000", // local dev
+        "*.vercel.app",        // regex: matches all preview deployments
+        "https://your-production-domain.com" // your prod domain
+        ],
     /*socialProviders: {
         //If I wanted to add Google or (perhaps Xero) as a way to sign in and log in
     }, */
