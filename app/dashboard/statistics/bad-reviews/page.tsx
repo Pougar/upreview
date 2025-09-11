204ba0d5e557f7ae10ef887eef19b6e438525f84
Nayan Gallego Sivaraman <nayan@Vijays-MacBook-Pro.local>
2025-09-12 02:01:09 +1000

    stats prettify

---
+++ app/dashboard/statistics/bad-reviews/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+
+export default function BadReviews() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>Bad Reviews Page</h1>
+    </div>
+  );
+}
